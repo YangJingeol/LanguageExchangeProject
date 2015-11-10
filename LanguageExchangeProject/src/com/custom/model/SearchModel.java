f5bd3eaf423a58625dfa3dc6af73d5b01d65c991
zoosix <homework4181@naver.com>
2015-11-11 05:23:35 +0900

    add custom.model

---
+++ LanguageExchangeProject/src/com/custom/model/SearchModel.java
@@ -0,0 +1,28 @@
+package com.custom.model;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.meeting.dao.MeetingDTO;
+import com.member.dao.MemberDAO;
+import com.member.dao.gtableDTO;
+
+public class SearchModel implements Model{
+
+	@Override
+	public String handlerRequest(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		// TODO Auto-generated method stub
+		String word;
+		word = req.getParameter("word");
+		System.out.println(word);
+		List<MeetingDTO> list=MemberDAO.searchMeeting(word);
+		req.setAttribute("list", list);
+		req.setAttribute("slide", "../main/slide.jsp");
+		req.setAttribute("jsp", "../main/meeting_list.jsp");
+		
+		return "main/main.jsp";
+	}
+
+}
